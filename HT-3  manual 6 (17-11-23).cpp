fe544e97b378974d9bee85474ab3ff5f7d33b9ad
TalhaMateen <147513600+TalhaMateen@users.noreply.github.com>
2023-11-28 21:30:47 +0500

    Add files via upload

---
+++ HT-3  manual 6 (17-11-23).cpp
@@ -0,0 +1,13 @@
+// program for floyd's triangle 
+#include <iostream>
+using namespace std;
+int main() {
+  cout<<1<<endl;
+for(int i=1; i<=7; i++){
+	for(int j=1; j<=i; j++){
+		if(i%2==0){	cout<<i<<"\t";}
+}
+   cout<<endl;
+} 
+ return 0;
+}
